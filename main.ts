2164b4f2f9c4bafc2166620d72a5457e87dc3624
Ethan-Choy <ethan.choy@stu.ocsb.ca>
2025-10-22 13:01:54 -0400

    init

--- main.ts
+++ main.ts
@@ -1,8 +1,35 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ethan Choy
+ * Created on: Sep 2025
+ * This program turns motor
 */
 
-basic.showString('Hello, World!')
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// variables
+const servoNumber1 = robotbit.Servos.S1
+
+// go to 0 degrees
+input.onButtonPressed(Button.A, function() {
+    basic.clearScreen()
+    robotbit.Servo(servoNumber1, 0)
+    basic.clearScreen()
+    basic.showString("0")
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
+
+// go to 180 degrees
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    robotbit.Servo(servoNumber1, 180)
+    basic.clearScreen()
+    basic.showString("180")
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
